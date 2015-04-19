5a7f8e838efed640ab58e25c3b71779766e246d0
sakusa <Tomoya393@gmail.com>
2015-04-19 22:13:43 +0900

    remove if($WixID!=0)

--- newBody.php
+++ newBody.php
@@ -11,30 +11,61 @@ require_once( dirname( __FILE__ ) . '/patternMatching.php' );
 add_filter( 'the_excerpt', 'new_body' );
 add_filter( 'the_content', 'new_body' );
 // remove_all_filters( 'the_content' );
+remove_filter('the_content', 'wptexturize'); //-などの特殊文字への変換を停止
 
-function new_body( $content, $innerLinkArray = '' ) {
-
+function new_body( $content, $innerLinkArray = '') {
 	if( is_preview() == false ) {
+
+		//プレビューと違ってEnterキーによる空白(空行)を1文字としてカウントしてしまって、preview_attachとattachが噛み合わなくなってたからこうしている。
+		$content = preg_replace('/(\s|　)/','',$content);
 
 		$patternMatching = new patternMatching;
 		$WixID = $patternMatching -> returnWixID();
 
-		if ( $WixID != 0 ) {
+		// if ( $WixID != 0 ) {
+			if ( !empty($innerLinkArray) ) {
 
-			$attachURL = 'http://trezia.db.ics.keio.ac.jp/sakusa_WIXServer_0.3.5/attach';
-			
-			// 新しい cURL リソースを作成
-			$ch = curl_init();
+				$attachURL = 'http://trezia.db.ics.keio.ac.jp/sakusa_WIXServer_0.3.5/PreviewAttach';
+				
+				// 新しい cURL リソースを作成
+				$ch = curl_init();
+// $tmp = '{ "0": { "end": [ "2" ], "nextStart": [ "5" ], "keyword": [ "女優" ], "targets": [ "http://aqua.db.ics.keio.ac.jp" ] } }';
+				// パラメータ	
+				$data = array(
+				    'minLength' => 3,
+				    'rewriteAnchorText' => 'false',
+				    'bookmarkedWIX' => $WixID,
+				    'body' => mb_convert_encoding($content, 'UTF-8'),
+				    'innerLinkArray' => $innerLinkArray
+				);
+				$data = http_build_query($data, "", "&");
 
-			// パラメータ	
-			$data = array(
-			    'minLength' => 3,
-			    'rewriteAnchorText' => 'false',
-			    'bookmarkedWIX' => $WixID,
-			    'body' => mb_convert_encoding($content, 'UTF-8'),
-			    'innerLinkArray' => $innerLinkArray,
-			);
-			$data = http_build_query($data, "", "&");
+			} else {
+
+				$DecideFileInfo = '';
+
+				if ( $pointer = opendir(WixDecideFiles) ) {
+					global $id;
+					while ( ($file = readdir($pointer)) !== false ) {
+						if ( $file === $id.'.txt' ) {
+							$DecideFileInfo = json_encode(DecideFileInfo(WixDecideFiles.$file), JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+							break;
+						}
+					}
+					closedir($pointer);
+				}
+
+				$attachURL = 'http://trezia.db.ics.keio.ac.jp/sakusa_WIXServer_0.3.5/attach';
+				$ch = curl_init();
+				$data = array(
+				    'minLength' => 3,
+				    'rewriteAnchorText' => 'false',
+				    'bookmarkedWIX' => $WixID,
+				    'body' => mb_convert_encoding($content, 'UTF-8'),
+				    'decideFileInfo' => $DecideFileInfo,
+				);
+				$data = http_build_query($data, "", "&");
+			}
 
 			try {
 				//送信
@@ -47,31 +78,23 @@ function new_body( $content, $innerLinkArray = '' ) {
 				$response = curl_exec($ch);
 
 				if ( $response === false ) {
-
 				    // エラー文字列を出力する
-				    echo 'エラーです. http_test.php';
-			    	echo curl_error( $ch );
-
+				    $response = 'エラーです. newBody.php-> ' .curl_error( $ch );
 				}
-
 			} catch ( Exception $e ) {
-			
-				echo '捕捉した例外: ',  $e -> getMessage(), "\n";
-			
+				$response = '捕捉した例外: ' . $e -> getMessage() . "\n";
 			} finally {
-
 				curl_close($ch);
-			
 			}
 
-			// return $content;
+			// $response = $content;
 			return $response;
 
-		} else {
+		// } else {
 
-			return $content;
+		// 	return $content;
 
-		}
+		// }
 		
 	} else {
 		
@@ -79,4 +102,38 @@ function new_body( $content, $innerLinkArray = '' ) {
 	
 	}
 }
+
+function DecideFileInfo($filename) {
+	$returnValue = array();
+
+	$file = fopen($filename, 'r');
+
+	if ( flock($file, LOCK_SH) ){
+		while ( !feof($file) ) {
+			$line = fgets($file);
+
+			if ( $line === false ) break;
+
+			$pieces = explode(',', $line);
+
+			$start = explode(':' ,$pieces[0])[1];
+			$end = explode(':' ,$pieces[1])[1];
+			$nextStart = explode(':' ,$pieces[2])[1];
+			$keyword = explode(':' ,$pieces[3])[1];
+			$target = trim( substr($pieces[4], strpos($pieces[4], ':')+1) );
+
+			$returnValue[$start] = array('end'=>$end, 'nextStart'=>$nextStart, 'keyword'=>$keyword, 'target'=>$target);
+
+		}
+	}
+
+	fclose($file);
+
+	return $returnValue;
+
+}
+
+
+
+
 ?>
